5d15a778766ddc05799f220954e8b8c98b82d9c3
Vikas Chouhan <vikaschouhan360@gmail.com>
2025-03-22 11:53:15 +0530

    tan stack query provider added for infinite scrolling (#3)
    Co-authored-by: unknown <vikas.chouhan@logichive.in>

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,3 +1,4 @@
+import QueryProvider from "@/utils/queryProvider";
 import React from "react";
 import PropertyCardSkeleton from "./skeleton";
 
@@ -5,6 +6,9 @@ const Page = () => {
   return (
     <div className="md:px-32 px-2 py-16 mt-12 max-w-[1920px] w-full">
       <PropertyCardSkeleton />
+      <QueryProvider>
+        <div></div>
+      </QueryProvider>
     </div>
   );
 };
